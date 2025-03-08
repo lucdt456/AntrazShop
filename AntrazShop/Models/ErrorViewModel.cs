0571702323f2f2ee39d3b59741ca572da64c4f3b
lucdt456 <lucdt456@gmail.com>
2025-03-08 20:18:49 +0700

    Add project files.

---
+++ AntrazShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AntrazShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
