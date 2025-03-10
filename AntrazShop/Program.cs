4c88f0b7711542f54d0e0809c70913686f3474de
lucdt456 <lucdt456@gmail.com>
2025-03-10 13:59:54 +0700

    a

--- AntrazShop/Program.cs
+++ AntrazShop/Program.cs
@@ -50,6 +50,10 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+	name: "Areas",
+	pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
